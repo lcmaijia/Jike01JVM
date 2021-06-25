81cc421607c26c236a2ae2f77fcf2a68bf096d47
lichangmin <1262497616@qq.com>
2021-06-25 23:28:55 +0800

    作业二

--- com/src/Classloader.java
+++ com/src/Classloader.java
@@ -1,8 +1,41 @@
-import java.net.URISyntaxException;
+package src;
 
-public class Classloader {
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-    public static void main(String[] args) throws URISyntaxException {
+public class Classloader extends ClassLoader {
 
+    public static void main(String[] args) {
+
+        Class<?> helloClass = new Classloader().findClass("Hello");
+        try {
+            Method method = helloClass.getMethod("hello");
+            try {
+                method.invoke(helloClass.newInstance());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    protected Class<?> findClass(String name) {
+        Path path = Paths.get("/JikeTime/WorkSpace/week01/Jike01JVM/com/src/files/Hello.xlass");
+        byte[] bytes64 = new byte[0];
+        try {
+            bytes64 = Files.readAllBytes(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < bytes64.length; i++) {
+            bytes64[i] = (byte)(255 - bytes64[i]);
+        }
+        return defineClass(name, bytes64, 0, bytes64.length);
     }
 }
