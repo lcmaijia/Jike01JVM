9b29b29721c9b0d4c372f5e14755d83dd0753f50
lichangmin <1262497616@qq.com>
2021-06-25 23:33:35 +0800

    作业二

--- com/src/Classloader.java
+++ com/src/Classloader.java
@@ -1,12 +1,15 @@
 package src;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+/**
+ * 自定义一个 Classloader，加载一个 Hello.xlass 文件，
+ * 执行 hello 方法，此文件内容是一个 Hello.class 文件所有字节（x=255-x）处理后的文件。
+ */
 public class Classloader extends ClassLoader {
 
     public static void main(String[] args) {
